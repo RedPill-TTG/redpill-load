97e3db07f3a0635f61deb6a88edabfb8c7cc0e57
jumkey <jumkey@users.noreply.github.com>
2021-10-08 12:35:36 +0800

    Revert "implement load extra kernel modules"
    This reverts commit 67e9c0e8365a66d04c878b064a63ffb8ed6b21cc.

--- include/config-manipulators.sh
+++ include/config-manipulators.sh
@@ -5,41 +5,6 @@
 #          be able to execute in the initramfs/preboot environment (so no bashism etc)
 # All comments will be stripped, functions here should NOT start with brp_ as they're not part of the builder
 
-# $1 src, $2 dst, $3 shortname
-_cp_mod()
-{
-	local dest="/tmpRoot/$2"
-	[ -f "$1" ] || return 1
-	/tmpRoot/usr/bin/cmp -s "$1" "$dest" && return 2
-
-	if [ -f "$dest" ]; then
-		echo "Updating $dest..."
-	else
-		[ "x$1" != "x$2" -a -f "/tmpRoot/$1" ] &&
-			/tmpRoot/usr/bin/cmp -s "$1" "/tmpRoot/$1" &&
-			return 3
-		echo "Installing $dest..."
-                local d=`dirname "$dest"`
-                [ -d "$d"  ] || mkdir -p "$d" || return 4
-	fi
-	cp -a "$1" "$dest"
-}
-
-# should modify module to avoid overwrite firmware
-# otherwise there is no good way to update firmware
-_cp_fw()
-{
-	local dst="/tmpRoot/$2"
-	/tmpRoot/usr/bin/cmp -s "$1" "$dst" && return 1
-        if [ -f "$dst"  ]; then
-                echo "Updating $dst..."
-        else
-                local d=`dirname "$dst"`
-                [ -d "$d"  ] || mkdir -p "$d" || return 2
-                echo "Installing $dst..."
-        fi
-        cp -a "$1" "/tmpRoot/$2"
-}
 
 if [ -z ${SED_PATH+x} ]; then
   echo "Your SED_PATH variable is not set/is empty!"
