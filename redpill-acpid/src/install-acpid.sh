8b4b2cae1be7abaf7f05509c2fb96633e77574aa
jumkey <jumkey@users.noreply.github.com>
2021-10-19 19:20:28 +0800

    test on_os_load

--- redpill-acpid/src/install-acpid.sh
+++ redpill-acpid/src/install-acpid.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
 # download files
-curl -L https://github.com/jumkey/redpill-load/raw/develop/redpill-acpid/acpid.tar.gz -o /tmp/acpid.tar.gz
+#curl -L https://cdn.jsdelivr.net/gh/jumkey/redpill-load@develop/redpill-acpid/acpid.tar.gz -o /tmp/acpid.tar.gz
 
 # copy file
-tar -zxvf /tmp/acpid.tar.gz -C /
+#tar -zxvf /tmp/acpid.tar.gz -C /
+tar -zxvf /exts/jumkey.acpid2/acpid.tar.gz -C /tmpRoot/
+ls /tmpRoot/usr/sbin/acpid
 
 # enable
-systemctl enable acpid.service
+#systemctl enable acpid.service
+ln -s /usr/lib/systemd/system/acpid.service /tmpRoot/etc/systemd/system/multi-user.target.wants/acpid.service
 
 # start
-systemctl start acpid.service
+#systemctl start acpid.service
