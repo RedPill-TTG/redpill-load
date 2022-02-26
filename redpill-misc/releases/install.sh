9d6c7191dd6d056c77bf7c45ce20ca7a056fe532
jumkey <jumkey@users.noreply.github.com>
2022-02-26 19:24:47 +0800

    fix sed path

--- redpill-misc/releases/install.sh
+++ redpill-misc/releases/install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-echo "Removing acpi cpufreq module"
-if (grep -r -q -E "(QEMU|VirtualBox)" /sys/devices/virtual/dmi/id/); then
+echo "Removing acpi CPUFreq module"
+if [ -e /tmpRoot/sbin/dmidecode ] && (/tmpRoot/sbin/dmidecode -s system-manufacturer | grep -q -E "(QEMU|VirtualBox)"); then
   echo "VM detected,remove acpi-cpufreq module"
-  sed -i 's/acpi-cpufreq/# acpi-cpufreq/g' /tmpRoot/usr/lib/modules-load.d/70-cpufreq-kernel.conf
+  /tmpRoot/usr/bin/sed -i 's/acpi-cpufreq/# acpi-cpufreq/g' /tmpRoot/usr/lib/modules-load.d/70-cpufreq-kernel.conf
 else
   echo "*No* VM detected,NOOP"
 fi
