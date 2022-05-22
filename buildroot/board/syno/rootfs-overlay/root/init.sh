f77ece8c33ff51d1ba11cbb8eb7de0a6269aabe6
jumkey <jumkey@users.noreply.github.com>
2022-05-22 23:01:12 +0800

    identify synoboot

--- buildroot/board/syno/rootfs-overlay/root/init.sh
+++ buildroot/board/syno/rootfs-overlay/root/init.sh
@@ -1,25 +1,63 @@
 #!/bin/bash
 
-cat /proc/cmdline | grep -q "whosyourdaddy"
-if [ $? -eq 0 ]; then
+if grep </proc/cmdline -q "whosyourdaddy"; then
+  # TODO add ttyd and lrzsz, first need some nic driver
   echo "Debug mode, you will stay at the first kernel"
   exit 0
 fi
 
-cd $(dirname $0)
+# Args: $1 vid $2 pid $3 synoboot_satadom $4 dom_szmax
+get_synoboot() {
+  local usbfs
+  if [[ -v $3 ]]; then
+    dev=$(udevadm trigger -v -n -s block -p ID_VENDOR_ID="$1" -p ID_MODEL_ID="$2" | head -n 1)
+    usbfs=/dev/"${dev##*/}"
+  else
+    # satadom size <= 1024 MB or dom_szmax
+    devs=$(find /sys/block/sd*)
+    for dev in $devs; do
+      if [[ $(cat "$dev"/size) -le $((${4-1024} * 1024 * 1024 / 512)) ]]; then
+        usbfs=/dev/"${dev##*/}"
+        break
+      fi
+    done
+  fi
+  echo "$usbfs"
+}
+
+CMDLINE=$(cat /proc/cmdline)
+vid=$(for x in $CMDLINE; do
+  [[ $x = vid=* ]] || continue
+  echo "${x#vid=}" | cut -b 3-6
+done)
+pid=$(for x in $CMDLINE; do
+  [[ $x = pid=* ]] || continue
+  echo "${x#pid=}" | cut -b 3-6
+done)
+synoboot_satadom=$(for x in $CMDLINE; do
+  [[ $x = synoboot_satadom=* ]] || continue
+  echo "${x#synoboot_satadom=}"
+done)
+dom_szmax=$(for x in $CMDLINE; do
+  [[ $x = dom_szmax=* ]] || continue
+  echo "${x#dom_szmax=}"
+done)
+
+cd "$(dirname "$0")" || exit
 mkdir /temp1
 mkdir /temp2
-# TODO sdb?
-usbfs=$(fdisk -l /dev/sd* | grep -E '128 MB|160 MB|1024 MB' | awk '{print $2}' | awk -F ':' '{print $1}')
+
+declare usbfs
+usbfs=$(get_synoboot "$vid" "$pid" "$synoboot_satadom" "$dom_szmax")
 wait_time=30
 time_counter=0
 while [ "${usbfs}" = "" ] && [ $time_counter -lt $wait_time ]; do
   sleep 1
-  usbfs=$(fdisk -l /dev/sd* | grep -E '128 MB|160 MB|1024 MB' | awk '{print $2}' | awk -F ':' '{print $1}')
-  echo "Still waiting for boot device (waited $((time_counter=time_counter+1)) of ${wait_time} seconds)"
+  usbfs=$(get_synoboot "$vid" "$pid" "$synoboot_satadom" "$dom_szmax")
+  echo "Still waiting for boot device (waited $((time_counter = time_counter + 1)) of ${wait_time} seconds)"
 done
-mount ${usbfs}1 /temp1
-mount ${usbfs}2 /temp2
+mount "${usbfs}"1 /temp1
+mount "${usbfs}"2 /temp2
 
 sh bzImage-to-vmlinux.sh /temp2/zImage vmlinux
 
@@ -28,14 +66,15 @@ rm vmlinux
 php patch-ramdisk-check.php vmlinux-mod vmlinux.bin
 rm vmlinux-mod
 
-mkdir "ramdisk" ; cd "ramdisk"
+mkdir "ramdisk"
+cd "ramdisk" || exit
 
-unlzma < /temp2/rd.gz | cpio -idmuv
+unlzma </temp2/rd.gz | cpio -idmuv
 
-unlzma < /temp1/custom.gz | cpio -idmuv
+unlzma </temp1/custom.gz | cpio -idmuv
 
 # rd.gz + custom.gz
-find . 2>/dev/null | cpio -o -H newc -R root:root | xz -9 --format=lzma > ../rd.gz
+find . 2>/dev/null | cpio -o -H newc -R root:root | xz -9 --format=lzma >../rd.gz
 
 cd ..
 
